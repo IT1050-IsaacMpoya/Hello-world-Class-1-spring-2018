56bb0a06851bcc305d5c26c08b9fa5b40820ab95
IT1050-IsaacMpoya <36143852+IT1050-IsaacMpoya@users.noreply.github.com>
2018-02-06 19:42:04 -0500

    Add files via upload

---
+++ IsaacMpoya lab1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1.cs
+{
+    class Program
+      
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My name is Isaac Mpoya");// my first program.....
+        }
+    
+    }
+}
